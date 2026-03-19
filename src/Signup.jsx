82ae15bc6824fc391a98b611f2bbda0db587142c
pavan <rekapallipavan83@gmail.com>
2026-03-19 14:25:50 +0530

    login page

---
+++ src/Signup.jsx
@@ -0,0 +1,8 @@
+function SignUp() {
+
+    return (
+        <h1>Sign UP</h1>
+    )
+}
+
+export default SignUp
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Login from './Login.jsx'
+import SignUp from './Signup.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Login></Login>
+    <SignUp></SignUp>
   </StrictMode>,
 )
